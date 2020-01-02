5839f98491d1abb0fc1274f9c3567397b87ae6a3
bairs <1148414656@qq.com>
2020-01-02 09:09:10 +0800

    local first commit

---
+++ springboot/src/main/java/com/db/mysql/dao/JpaConfiguration.java
@@ -0,0 +1,22 @@
+package com.db.mysql.dao;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Order(Ordered.HIGHEST_PRECEDENCE)
+@Configurable
+@EnableTransactionManagement(proxyTargetClass = true)
+@EnableJpaRepositories(basePackages = "com.**.dao")
+@EntityScan(basePackages = "com.**.entity")
+public class JpaConfiguration {
+    @Bean
+    PersistenceExceptionTranslationPostProcessor persistenceExceptionTranslationPostProcessor() {
+        return new PersistenceExceptionTranslationPostProcessor();
+    }
+}
---
+++ springboot/src/main/java/com/db/mysql/dao/RoleRepository.java
@@ -0,0 +1,9 @@
+package com.db.mysql.dao;
+
+import com.db.mysql.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+}
